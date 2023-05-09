2864e1042aaf94609e23ca5e8eb4a68bbab462a7
이희재 <hj.lee2@stickint.kr>
2023-05-09 10:56:03 +0900

    타입스크립트 함수

--- ch01/hello.ts
+++ ch01/hello.ts
@@ -1,1 +1,11 @@
-console.log('Hello World')
+function add(num1: number, num2: number) {
+  console.log(num1 + num2)
+}
+add(1, 2)
+
+function showItems(arr: number[]) {
+  arr.forEach((item) => {
+    console.log(item)
+  })
+}
+showItems([1, 2, 3])
