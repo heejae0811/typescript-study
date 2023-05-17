4431da1fb054e3ae6491c25288a7e74031427404
이희재 <hj.lee2@stickint.kr>
2023-05-17 18:15:07 +0900

    타입스크립트 (비)구조화, ...연산자

---
+++ ch01/spread.ts
@@ -0,0 +1,24 @@
+// 잔여 연산자
+let address:any = {
+  country: '대한민국',
+  city: '서울',
+  address1: '양천구',
+  address2: '목동서로',
+  address3: '221'
+}
+
+const {country, city, ...detail} = address
+
+console.log(country)
+console.log(city)
+console.log(detail)
+
+// 전개 연산자
+// @ts-ignore
+let name = {name: 'Jack'}
+let age = {age: 30}
+
+// @ts-ignore
+const userInfo = {...name, ...age}
+
+console.log(userInfo)
---
+++ ch01/structuring.ts
@@ -0,0 +1,33 @@
+// 구조화
+export interface IPerson {
+  name: string
+  age: number
+}
+
+export interface ICompany {
+  name: string
+  age: number
+}
+
+let jack:IPerson = {
+  name: 'Jack',
+  age: 29
+}
+
+let naver:ICompany = {
+  name: 'Naver',
+  age: 10
+}
+
+console.log(jack)
+console.log(naver)
+
+// 비구조화
+let jack_name = jack.name
+let jack_age = jack.age
+
+console.log(jack_name, jack_age)
+
+let {name, age} = naver
+
+console.log(name, age)
